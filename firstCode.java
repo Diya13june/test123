b524937f38e2d599702b5168e3be8bd858cc9fd5
Diya13june <165755262+Diya13june@users.noreply.github.com>
2024-04-02 17:44:21 +0530

    Add files via upload

---
+++ firstCode.java
@@ -0,0 +1,22 @@
+package practice;
+
+public class firstCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Arithmetic Operations
+		int a=10;
+		int b=20;
+		System.out.println("a+b=" + (a+b));
+		System.out.println("a-b=" + (a-b));
+		for(int i=0; i<5; i++)//for loop
+			System.out.println(i);
+		int i=0;
+		do {
+			System.out.println(i);
+			i++;
+		}while(i<=10);
+
+	}
+
+}
